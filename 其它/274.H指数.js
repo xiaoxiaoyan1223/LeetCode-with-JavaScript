e256cfafb12bb073cd99f85f7f832ab56843118e
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-09-01 17:04:49 +0800

    Add files via upload

---
+++ 其它/274.H指数.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    citations.sort((a,b)=>a-b)
+    let h=0,i=citations.length-1
+    while(i>=0&&citations[i]>h){
+    h++
+    i--
+    }
+    return h
+};
