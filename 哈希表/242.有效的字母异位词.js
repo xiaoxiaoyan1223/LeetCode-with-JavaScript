b3daf1c121d9daa95b76b68dc363477a24d3d152
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-09-09 19:56:11 +0800

    Add files via upload

---
+++ 哈希表/242.有效的字母异位词.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    //方法一
+    // return s.length == t.length && [...s].sort().join('') == [...t].sort().join('')
+    //方法二
+    let len1=s.length
+    let len2=t.length
+    if(len1!==len2) return false
+    const map=new Map()
+    for(i of s){
+       map.get(i)?map.set(i,map.get(i)+1):map.set(i,1)
+    }
+    for(i of t){
+        if(!map.get(i)){
+            return false
+        }else{
+            map.set(i,map.get(i)-1)
+            if(map.get(i)<0) {
+                return false
+            }
+        }
+    }
+    return true
+};
