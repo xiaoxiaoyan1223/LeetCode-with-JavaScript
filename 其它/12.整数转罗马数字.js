457938b4fd9013d993bcf8fa8dadb76da5b88779
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-11-15 19:49:59 +0800

    Add files via upload

---
+++ 其它/12.整数转罗马数字.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+     let intArr=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    let RomanArr=["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    let index=0;
+    let res="";
+    while(index<13){
+        while(num>=intArr[index]){
+            res+=RomanArr[index];
+            num-=intArr[index];
+        }
+        index++;
+    }
+    return res
+};
