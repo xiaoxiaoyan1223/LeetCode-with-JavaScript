a9e158368d837e74ad76d95d94e8cd1b133c5f16
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-12-24 17:34:18 +0800

    Update 3.无重复字符的最长长度.js

--- 哈希表/3.无重复字符的最长长度.js
+++ 哈希表/3.无重复字符的最长长度.js
@@ -3,10 +3,14 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    // 使用 Map 数据结构来存储字符和其对应的索引
     const map=new Map()
+    // 左指针，表示当前无重复字符子串的起始位置
     let l=0;
     let num=0
     for(let i=0;i<s.length;i++){
+        // 如果当前字符已经在子串中出现过，并且其索引大于等于左指针 l，
+        // 则更新左指针 l，使其指向重复字符的下一个位置
         if(map.has(s[i])&&map.get(s[i])>=l){
             l=map.get(s[i])+1
         }
