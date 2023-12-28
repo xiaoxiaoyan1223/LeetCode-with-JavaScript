ee2a63ce280489ebe966d8329b6163c210aa7285
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2023-12-28 18:27:10 +0800

    Update 560.和为k的子数组个数.js

--- 其它/560.和为k的子数组个数.js
+++ 其它/560.和为k的子数组个数.js
@@ -4,20 +4,18 @@
  * @return {number}
  */
     var subarraySum = function(nums, k) {
-    let res = 0;
-    let map = new Map;
-    //保存当前位置的前缀和
-    let sum = 0;
-    //前缀和存储到map中
-    map.set(0,1);
-    for(let i = 0; i < nums.length; i++){
-        //因为第一个前缀和已处理，所以跳过第一个
-        if(i !== 0){
-            sum += nums[i - 1];
-            map.set(sum, map.has(sum) ? map.get(sum) + 1 : 1);
-        }
-        //查看和（累加到当前值 - k）相等的i之前的前缀和有多少个，并累加到结果中
-        res += map.has(sum + nums[i] - k) ? map.get(sum + nums[i] - k) : 0;
+  let res = 0; // 存储结果的变量
+  let map = new Map(); // 使用 Map 数据结构来存储前缀和及其出现次数
+  let sum = 0; // 当前位置的前缀和
+  map.set(0, 1); // 将初始的前缀和 0 存入 map 中，出现次数为 1
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== 0) {
+      // 计算当前位置的前缀和，并将其存入 map 中
+      sum += nums[i - 1];
+      map.set(sum, map.has(sum) ? map.get(sum) + 1 : 1);
     }
-    return res;
+    // 查看和（累加到当前值 - k）相等的 i 之前的前缀和有多少个，并累加到结果中
+    res += map.has(sum + nums[i] - k) ? map.get(sum + nums[i] - k) : 0;
+  }
+  return res; // 返回结果
 };
