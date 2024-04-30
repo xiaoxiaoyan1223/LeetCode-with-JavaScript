a50f500ed47d3190ea1d8f67a8c7386c89e53f25
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-04-30 08:34:11 +0800

    Update 205.同构字符串.js

--- 哈希表/205.同构字符串.js
+++ 哈希表/205.同构字符串.js
@@ -14,3 +14,32 @@ var isIsomorphic = function(s, t) {
     }
     return true
 };
+
+// 方法二
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const len1=s.length
+    const len2=t.length
+    if(len1!==len2) return false
+    const map1=new Map()
+    const map2=new Map()
+    for(let i=0;i<len1;i++){
+        if(map1.has(s[i])){
+            if(map1.get(s[i])!==t[i]) return false
+        }else{
+            map1.set(s[i],t[i])
+        }
+        // 只建立单向映射不能满足类似 s="badc" t="baba"
+        // 所以我们要建立双向映射
+        if(map2.has(t[i])){
+            if(map2.get(t[i])!==s[i]) return false
+        }else{
+            map2.set(t[i],s[i])
+        }
+    }
+    return true
+};
