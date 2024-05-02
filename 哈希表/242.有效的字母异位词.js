fb2890df9071aa797f307434e2be8a0dca595f9c
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-05-02 11:05:55 +0800

    Update 242.有效的字母异位词.js

--- 哈希表/242.有效的字母异位词.js
+++ 哈希表/242.有效的字母异位词.js
@@ -26,3 +26,14 @@ var isAnagram = function(s, t) {
     }
     return true
 };
+// 方法三
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    return s.split('').sort().join("")===t.split('').sort().join("")
+    
+};
