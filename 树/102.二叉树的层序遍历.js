16f837f70f0860fcb0c81831e9646925fe6c3e74
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-05-08 19:41:07 +0800

    Update 102.二叉树的层序遍历.js

--- 树/102.二叉树的层序遍历.js
+++ 树/102.二叉树的层序遍历.js
@@ -34,3 +34,39 @@ var levelOrder = function(root) {
     }
     return result
 };
+
+方法二  思路一样只是更清晰
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    //需要借助队列模拟
+let res=[],queue=[]
+queue.push(root)
+if(root===null) return res
+while(queue.length!==0){
+    // 记录当前层级节点数
+    let length=queue.length
+    // 存放每一层的节点数
+    let curLevel=[]
+    for(let i=0;i<length;i++){
+        // 把队头节点删除
+        let node=queue.shift()
+        curLevel.push(node.val)
+        node.left&&queue.push(node.left)
+        node.right&&queue.push(node.right)
+    }
+    // 把每一层的结果放到结果数组里面
+    res.push(curLevel)
+} 
+    return res
+};
