9bcf28edddb57a3cfa340aaf99121847b0c69e33
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-06-05 17:46:29 +0800

    Update 129.求根节点到叶子节点数字之和.js

--- 树/129.求根节点到叶子节点数字之和.js
+++ 树/129.求根节点到叶子节点数字之和.js
@@ -24,4 +24,13 @@ var sumNumbers = function(root) {
     }
     dfs(root,'')
     return res
+
+    // 方法二
+     const inOrder=(root,sum)=>{
+    if(root===null) return 0
+    sum=sum*10+root.val
+    if (root.left === null && root.right === null) return sum;
+    return inOrder(root.left, sum) + inOrder(root.right, sum);
+   }
+   return inOrder(root,0)
 };
